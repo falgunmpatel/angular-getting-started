40cc6061f53298257c2c8792a2c8d121f83eb633
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-22 12:03:22 +0530

    [Falgun Patel] UC4 - display Hello from BridgeLabz with a user inputted name
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -11,6 +12,7 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = '../assets/BL_logo_square_jpg.jpg';
   url = 'https://www.bridgelabz.com';
+  username: string = '';
 
   ngOnInit() {
     console.log('Hello from BridgeLabz');

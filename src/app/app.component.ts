be9824697b6a2cf36acb3faed63f66c3710ada96
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-22 12:28:46 +0530

    [Falgun Patel] UC5 - ensure userName input is valid and display error message
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   imgUrl = '../assets/BL_logo_square_jpg.jpg';
   url = 'https://www.bridgelabz.com';
   username: string = '';
+  nameError: string = '';
 
   ngOnInit() {
     console.log('Hello from BridgeLabz');
@@ -21,5 +22,15 @@ export class AppComponent {
   onClick($event: any) {
     console.log('Save button is clicked!', $event);
     window.open(this.url, '_blank');
+  }
+
+  onInput($event: any) {
+    console.log('Change Event Occured!', $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.username)) {
+      this.nameError = '';
+      return;
+    }
+    this.nameError = 'Name is Incorrect!';
   }
 }
